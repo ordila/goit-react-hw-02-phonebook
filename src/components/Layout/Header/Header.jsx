672b315282b01b84f1b6e5cd731433820a99bd8e
ordila <ordila99@gmail.com>
2023-10-16 17:25:43 +0300

    gen

--- src/components/Layout/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-export const Header = ({ children }) => {
-  return (
-    <header className="pb-4">
-      <h1>{children}</h1>
-    </header>
-  );
-};
--- src/components/Layout/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Header/Header';
-export * from './Sidebar/Sidebar';
-export * from './Layout';
